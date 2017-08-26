08f27064630ca961e2b9fddb7f7f57163db4bd2d
hamiltons4 <smithhamilton4@gmail.com>
2017-08-26 16:10:56 -0500

    linked photo cards to message map

--- src/frontend/src/views/PhotoPageView.js
+++ src/frontend/src/views/PhotoPageView.js
@@ -23,31 +23,31 @@ class PhotoPageView extends React.Component {
 			<div>
 			<main>
 				<h2>PhotoPageView</h2>
-				{data.allMessages.map(item => (
+			{/*	{data.allMessages.map(item => (
 					<p key={item.id}>
 						<Link to={'/messages/${item.id}/'}>
 							{item.message}
 						</Link>
 					</p>
-				))}
-					<a href="a" title="Post 1">
-					<article>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit inventore quo facere molestias doloribus, soluta ipsa.</p>
-						<figure>
-							<img src="http://via.placeholder.com/480x360" alt="image-1"></img>
-							<figcaption>Post 1</figcaption>
-						</figure>
-					</article>
-					</a>
-					<a href="a" title="Post 2">
-					<article>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit inventore quo facere molestias doloribus, soluta ipsa.</p>
-						<figure>
-							<img src="http://via.placeholder.com/400x360" alt="image-2"></img>
-							<figcaption>Post 2</figcaption>
-						</figure>
-					</article>
-					</a>
+				))} */}
+					
+						{data.allMessages.map(item => (
+							<a href="a" title="Post 1">
+							<article>
+								<p key={item.id}>
+									<Link to={'/messages/${item.id}/'}>
+										{item.message}
+									</Link>
+								</p>
+							<figure>
+								<img src="http://via.placeholder.com/480x360" alt="image-1"></img>
+								<figcaption>Post 1</figcaption>
+							</figure>
+							</article>
+							</a>				
+						))}
+						
+					
 			</main>
 
 			</div>
